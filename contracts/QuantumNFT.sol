03abe830dd460d54710bf0d5eba0e724f9d8b96c
Sian Liu <sianliu@gmail.com>
2023-07-14 16:02:09 +0800

    Initial commit QuantumNFT.sol

--- contracts/QuantumNFT.sol
+++ contracts/QuantumNFT.sol
@@ -2,40 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
 
-contract QuantumNFT is ERC721URIStorage {
-    using Strings for uint256;
-    using Counters for Counters.Counter; 
-    Counters.Counter private _circuitIds;
+abstract contract QuantumNFT is ERC721 {
+     
+    constructor() ERC721 ("QuantumNFT", "QNFT") {}
 
-    constructor() ERC721 ("QuantumNFT", "QNFT") {} 
-    
-    // stores NFT fully on-chain 
-    function generateCircuit() public view returns(string memory) {
-
-    bytes memory svg = abi.encodePacked(
-        'svg code goes here'
-    );
-    return string(
-        // creates the "URL" to view image in a browser
-        abi.encodePacked(
-            "data:image/svg+xml;base64,",
-            Base64.encode(svg)
-        )    
-    );
-    } // end generateCharacter
-
-    // assigns ownership
-    function mint() public {
-        _circuitIds.increment();
-        uint256 newCircuitId = _circuitIds.current();
-        _safeMint(msg.sender, newCircuitId);
-        // calls generateCircuit
-    } //end mint function
+    function mint(string memory circuit) public returns (uint256[] memory) {
+        // 1. hash circuit 
+        // 2. compute tokenId
+        // 3. calls generateCircuit(circuit)
+    }
 }
-
 
