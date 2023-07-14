51d2f77be2be8acbb2a4ced442efa15b86d64e28
Sian Liu <sianliu@gmail.com>
2023-07-14 17:02:25 +0800

    QuantumNFT.sol added circuitId functionality

--- contracts/QuantumNFT.sol
+++ contracts/QuantumNFT.sol
@@ -5,14 +5,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
-abstract contract QuantumNFT is ERC721 {
+contract QuantumNFT is ERC721 {
      
+    bytes16 private circuitId;
+
     constructor() ERC721 ("QuantumNFT", "QNFT") {}
 
-    function mint(string memory circuit) public returns (uint256[] memory) {
+    function mint(string memory circuit) public {
         // 1. hash circuit 
+        circuitId = bytes16(keccak256(bytes(circuit)));
+        
         // 2. compute tokenId
         // 3. calls generateCircuit(circuit)
+    }
+
+    function getCircuitId() public view returns (bytes16) {
+        return circuitId; 
     }
 }
 
